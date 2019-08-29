9d52e16ea7041c3eddb27355c574ede7f96fda6b
Charles DeLeo <charles.deleo979@gmail.com>
2019-08-29 22:00:38 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,11 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => "Grace Hopper"}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  {:key => "value, i can be any data type" }
 end
 
 def my_hash_creator(key, value)
